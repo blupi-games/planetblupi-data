8773d6f9fa5d690b1a0c34c20c7dea79e4c0d822
Mathieu Schroeter <mathieu@schroetersa.ch>
2023-11-22 23:15:07 +0100

    Adapt conv script for the music

--- musics/conv.sh
+++ musics/conv.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-echo "Configure VLC for using 'Timbres of Heaven' sf2 file (fluidsynth)"
-echo "provided by http://midkar.com/soundfonts/"
-echo "Change for a gain of 0.10"
+echo "Configure VLC to use the Windows soundfont 'GS sound set (16 bit).sf2"
+echo "Change for a gain of 0.20"
 
-for a in $(ls *.mid); do
-  cvlc "$a" --play-and-exit '--sout=#transcode{vcodec=none,acodec=flac,ab=512,channels=2,samplerate=44100}:std{access=file,mux=flac,dst=-' | ffmpeg -i pipe: -aq 0 ./${a%.mid}.ogg
+for a in *.mid; do
+  cvlc "$a" --play-and-exit '--sout=#transcode{vcodec=none,acodec=flac,ab=512,channels=2,samplerate=44100}:std{access=file,mux=flac,dst=-' | ffmpeg -i pipe: -aq 0 "./${a%.mid}.ogg"
 done
